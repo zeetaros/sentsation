f353a7725b40252b22aeb57d6a0494e8908fd546
Sam Yip <40538028+zeetaros@users.noreply.github.com>
2019-06-16 22:43:40 +0100

    Added requirements.txt

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='sentsation',
     version='0.0.1',
-    author='Sam Yip',
+    author='Cheuky S.Y',
     packages=['sentsation',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
